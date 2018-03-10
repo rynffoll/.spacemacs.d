d27c1152365ab91db1ec1b2e95c07c8486961175
Ruslan Kamashev <rynffoll@gmail.com>
2018-03-10 20:52:13 +0300

    Small refactoring of rynffoll-org layer

--- layers/rynffoll-org/packages.el
+++ layers/rynffoll-org/packages.el
@@ -19,8 +19,7 @@
         org-tags-column 0
         org-agenda-tags-column 0
         org-directory "~/Dropbox/Org"
-        org-default-notes-file (concat org-directory "/notes.org")
         org-default-todo-file (concat org-directory "/todo.org")
-        org-default-inbox-file (concat org-directory "/inbox.org")
-        org-agenda-files `(,org-default-todo-file ,org-default-inbox-file)
-        org-archive-location (concat org-directory "/archive.org" "::* From %s")))
+        org-default-notes-file (concat org-directory "/notes.org")
+        org-agenda-files `(,org-default-todo-file)
+        org-archive-location (concat org-directory "/old/archive.org" "::* From %s")))
