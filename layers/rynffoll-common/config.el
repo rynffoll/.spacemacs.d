d3ec04883681d6301f29fbd7a9a9bc409a8a358a
Ruslan Kamashev <rynffoll@gmail.com>
2018-07-27 23:03:48 +0300

    Add WSL tweak for opening links

--- layers/rynffoll-common/config.el
+++ layers/rynffoll-common/config.el
@@ -11,3 +11,13 @@
 
 ;; input method
 (setq default-input-method "russian-computer")
+
+;; WSL tweaks
+(when (file-directory-p "/mnt/c/Windows/System32/cmd.exe")
+  ;; https://adam.kruszewski.name/2017/09/emacs-in-wsl-and-opening-links/
+  (let ((cmd-exe "/mnt/c/Windows/System32/cmd.exe")
+        (cmd-args '("/c" "start")))
+    (when (file-exists-p cmd-exe)
+      (setq browse-url-generic-program  cmd-exe
+            browse-url-generic-args     cmd-args
+            browse-url-browser-function 'browse-url-generic))))
